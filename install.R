f400c308f35948ed2cf925223fc7ed8b2a01cd3b
JamesStagge <jhstagge@gmail.com>
2018-10-22 08:07:29 -0400

    Added install and runtime

---
+++ install.R
@@ -0,0 +1,23 @@
+# *------------------------------------------------------------------
+# | PROGRAM NAME: 00_prepare_file_system
+# | FILE NAME: 00_prepare_file_system.R
+# | DATE: 
+# | CREATED BY:  Jim Stagge         
+# *----------------------------------------------------------------
+# | PURPOSE:  This code installs all packages needed for lecture code
+# | 
+# | 
+# |
+# *------------------------------------------------------------------
+
+###########################################################################
+###  Install packages 
+###########################################################################
+### Packages from CRAN
+install.packages("tidyverse")
+install.packages("devtools")
+
+### Packages from GitHub
+#require(devtools)
+
+
